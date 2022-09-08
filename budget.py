4085a35b7d41d117581a602f8ddf78566b699245
Alvin <alvin.tol@hotmail.com>
2022-09-08 10:48:54 -0600

    adds deposit function to Category class

--- budget.py
+++ budget.py
@@ -3,7 +3,11 @@ class Category:
     self.name = name
     self.ledger = list()
 
-
+  def deposit(self, amount, description = '') :
+    self.amount = amount
+    self.description = description
+    
+  
 
 
 def create_spend_chart(categories):
